4cdfaa13007cc382fb4828df7b9aba2d4f2d8c6e
Ankush Choudhary <kushchoudhary2611@gmail.com>
2022-03-30 10:50:21 +0530

    Travel Expence

--- app/src/main/java/com/example/Common/main_screen.java
+++ app/src/main/java/com/example/Common/main_screen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ import android.widget.ViewFlipper;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -29,6 +31,7 @@ public class main_screen extends AppCompatActivity implements NavigationView.OnN
     TextView textView;
     Menu menu;
     ViewFlipper flipper;
+    AppCompatButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,7 @@ public class main_screen extends AppCompatActivity implements NavigationView.OnN
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         flipper = (ViewFlipper) findViewById(R.id.flipper);
+        button = findViewById(R.id.predict);
         toolbar = findViewById(R.id.toolbar);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         navigationView.bringToFront();
@@ -51,6 +55,13 @@ public class main_screen extends AppCompatActivity implements NavigationView.OnN
         for (int i=0; i<imgarray.length; i++){
             showImage(imgarray[i]);
         }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(main_screen.this, Flights.class));
+            }
+        });
     }
 
     public void showImage(int img){
